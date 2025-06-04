81cb334c1e6c48974c4c9e26f3c73ff8b97cb872
nitrogenberry <131041571+xxyoohoo@users.noreply.github.com>
2025-06-04 15:04:40 -0400

    Update Final Vocab.cpp

--- French I/Final Vocab.cpp
+++ French I/Final Vocab.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 int main()
 {
-    cout << "Pick a category, and type the corresponding number:\n";
-    cout <<"1: La Ville & Les Directions\n" << "2: Les Adjectifs\n" << "3: La Maison & Les Meubles\n" << 
+    cout << "Pick ONE category, and type the corresponding number:\n";
+    cout <<"1: La Ville & Les Directions\n" << "2: Les Adjectifs\n" << "3: La Maison & Les Meubles\n" << "4: L\'ecole & Les matieres" << "5: -ER Verbs\n" << "6: -IR Verbs" << "7: -RE Verbs" << "8: La Famille";
     int choice;
+    cin >> choice;
     map<int,map<string,string>> vocab =
     {
         {1,{
@@ -38,7 +39,22 @@ int main()
             {"Attendre", "to wait"}, {"Entendre", "to hear"}, {"Perdre", "to lose"}, {"Vendre", "to sell"}, {"Répondre", "to answer"}}},
         {8,{
             {"Mes parents", "My parents"}, {"Mon ami", "My (male) friend"}, {"Mon amie", "My (female) friend"}, {"Mon frère", "My brother"}, {"Ma sœur", "My sister"},
-            {"Mon chien", "My dog"}, {"Mon chat", "My cat"}, {"Mon animal domestique", "My pet"}, {"Mon animal de compagnie", "My companion animal"}}}
+            {"Mon chien", "My dog"}, {"Mon chat", "My cat"}, {"Mon animal domestique", "My pet"}, {"Mon animal de compagnie", "My companion animal"}}},
+        {9,{
+            {"le petit déjeuner", "breakfast"}, {"le déjeuner", "lunch"}, {"le dîner", "dinner"}, {"le goûter", "snack"}, {"le pique-nique", "picnic"},
+            {"l'entrée", "starter"}, {"le plat principal", "main course"}, {"le fromage", "cheese"}, {"le dessert", "dessert"}, {"la salade", "salad"},
+            {"le steak-frites", "steak and fries"}, {"le croque monsieur", "grilled ham and cheese"}, {"la croque madame", "grilled ham and cheese with egg"},
+            {"le pain", "bread"}, {"l’oeuf", "egg"}, {"le citron", "lemon"}, {"la viande", "meat"}, {"le riz", "rice"},
+            {"les légumes", "vegetables"}, {"la glace", "ice cream"}, {"le croissant", "croissant"}, {"le soda", "soda"}, {"les pommes de terre", "potatoes"},
+            {"le beurre", "butter"}, {"le thon", "tuna"}, {"le saumon", "salmon"}, {"les crevettes", "shrimp"}, {"la mousse au chocolat", "chocolate mousse"},
+            {"la confiture", "jam"}, {"le poisson", "fish"}, {"le poulet", "chicken"}, {"le jambon", "ham"}, {"le boeuf", "beef"},
+            {"le porc", "pork"}, {"l’huile", "oil"}, {"l’oignon", "onion"}, {"les tomates", "tomatoes"}, {"les concombres", "cucumbers"},
+            {"les carottes", "carrots"}, {"le céleri", "celery"}, {"les pommes", "apples"}, {"les fraises", "strawberries"}, {"les bananes", "bananas"},
+            {"les oranges", "oranges"}, {"l’omelette", "omelet"}, {"les frites", "fries"}, {"le sel", "salt"}, {"le poivre", "pepper"},
+            {"le café", "coffee"}, {"la boisson", "drink"}, {"la limonade", "lemonade"}, {"avoir soif", "to be thirsty"}, {"avoir faim", "to be hungry"},
+            {"une livre", "a pound"}, {"un kilo", "a kilogram"}, {"un gramme", "a gram"}, {"un morceau", "a piece"}, {"une tranche", "a slice"},
+            {"un pot", "a jar"}, {"une boîte", "a can/box"}, {"une assiette", "a plate"}, {"un couteau", "a knife"}, {"une fourchette", "a fork"},
+            {"une cuillère à soupe", "a tablespoon"}, {"une cuillère à café", "a teaspoon"}, {"une tasse", "a cup"}, {"un verre", "a glass"}, {"une serviette", "a napkin"}}},
     };
     cout << "You picked: #" << choice << endl;
     if(guess=)
