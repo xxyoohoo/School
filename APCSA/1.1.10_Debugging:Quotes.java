ab7761ede9a09f19c8164a51fa43a647e218f131
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-09 20:40:58 -0400

    Create 1.1.10_Debugging:Quotes.java

---
+++ APCSA/1.1.10_Debugging:Quotes.java
@@ -0,0 +1,13 @@
+public class BuggyQuotes
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Why do programmers prefer dark mode?");
+        System.out.println("Because the light attracts bugs!");
+        System.out.println("Programming is a way of thinking, not just a way of writing code.");
+        System.out.println("-Donald Knuth");
+        System.out.println("No one in the brief history of computing has ever written a piece of perfect software.");
+        System.out.println("It's unlikely that you'll be the first.");
+        System.out.println("-Andrew Hunt");
+    }
+}
