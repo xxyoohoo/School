bab1e940d884b1b4198066efaefcf928ede5aeff
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-09 20:41:19 -0400

    Create 1.1.11_CompilingAndRunningJava.java

---
+++ APCSA/1.1.11_CompilingAndRunningJava.java
@@ -0,0 +1,22 @@
+/*Compile and Run the Java Program:
+
+1. Press the Run button. This will open up a command line prompt.
+
+2. Compile the program by entering the command `javac` followed by the name of the program: `javac Buggy.java`
+
+3. You will see an error because the program contains a bug. Press Stop to terminate the process and fix the error.
+
+4. Once you have fixed the error, repeat steps 1 & 2 to recompile the program. If you fixed the program correctly, no errors will appear after compilation.
+
+5. Once the program has successfully compiled, enter the command to run the program by entering `java` followed by the program name without the ".java" at the end: `java Buggy`
+
+6. You will know the program has successfully run when it prints "Hello world" to the console.
+*/
+
+public class Buggy
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+}
