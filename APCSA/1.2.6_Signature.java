5ff33dc5fd6a0bb5c35ebc853b073ddc3d2aaf59
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-09 20:43:16 -0400

    Create 1.2.6_Signature.java

---
+++ APCSA/1.2.6_Signature.java
@@ -0,0 +1,18 @@
+public class Signature
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Initials:");
+        char i1='A';
+        char i2='Y';
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println("Date:");
+        int mm=11;
+        int dd=7;
+        int yy=2011;
+        System.out.println(mm);
+        System.out.println(dd);
+        System.out.println(yy);
+    }
+}
