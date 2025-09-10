d724db198ba9aeb8c265b232ead4f7ad41899a73
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-09 20:44:08 -0400

    Create 1.2.11_VariablesAboutYou.java

---
+++ APCSA/1.2.11_VariablesAboutYou.java
@@ -0,0 +1,26 @@
+public class Variables 
+{
+    public static void main(String[] args)
+    {
+        // 1. Declare and initialize a variable for each scenario:
+        
+        // A friend's name
+        String name="Ziya";
+        // Your lucky number
+        int num=9;
+        // Whether or not you are you a morning person (the value 
+        // should be either `true` or `false`)
+        boolean morning=true;
+        // Your favorite snack
+        String snack="Pocky";
+        // The price of your favorite snack
+        double price=1.99;
+        // 2. Print out each variable:
+        System.out.println(name);
+        System.out.println(num);
+        System.out.println(morning);
+        System.out.println(snack);
+        System.out.println(price);
+        
+    }
+}
