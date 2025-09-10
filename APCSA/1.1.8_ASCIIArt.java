5d625f18d8c4fa6e07fa1b391380a5a02d45449f
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-09 20:40:37 -0400

    Create 1.1.8_ASCIIArt.java

---
+++ APCSA/1.1.8_ASCIIArt.java
@@ -0,0 +1,11 @@
+public class AsciiArt
+{
+    public static void main(String[] args)
+    {
+        System.out.println("   __         ");
+        System.out.println(" o-''|)_____/)");
+        System.out.println(" |__/|_)     )");
+        System.out.println("    |  __  /  ");
+        System.out.println("    (_/ (_/   ");
+    }
+}
