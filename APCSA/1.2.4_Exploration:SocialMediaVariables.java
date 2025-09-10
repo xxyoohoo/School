f7ec665f491a33de0f1c9873e81f921e02562a01
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-09 20:43:02 -0400

    Create 1.2.4_Exploration:SocialMediaVariables.java

---
+++ APCSA/1.2.4_Exploration:SocialMediaVariables.java
@@ -0,0 +1,32 @@
+public class SocialMediaVariables 
+{
+
+    public static void main(String[] args)
+    {
+        int snapchatsSent = 353;
+        System.out.println("Number of snapchats sent: ");
+        System.out.println(snapchatsSent);
+        
+        double youTubeVideosWatched = 130;
+        System.out.println("Number of YouTube videos watched: ");
+        System.out.println(youTubeVideosWatched);
+        
+        // Data updated two weeks later
+        snapchatsSent = 400;
+        System.out.println("Number of snapchats sent: ");
+        System.out.println(snapchatsSent);
+        
+        youTubeVideosWatched = 240.4;
+        System.out.println("Number of YouTube videos watched: ");
+        System.out.println(youTubeVideosWatched);
+        
+        // Write your Modify code here!
+        int instapost=0;
+        System.out.println("Number of Instagram Posts: ");
+        System.out.println(instapost);
+        instapost=10;
+        System.out.println("Number of Instagram Posts: ");
+        System.out.println(instapost);
+    }
+
+}
