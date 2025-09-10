390243fa0c480581a420dc4b54f4cc0f1249305d
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-09 20:43:51 -0400

    Create 1.2.10_SignatureWIthStrings.java

---
+++ APCSA/1.2.10_SignatureWIthStrings.java
@@ -0,0 +1,14 @@
+public class Signature
+{
+    public static void main(String[] args)
+    {
+        String first="Arianna";
+        String last="Yang";
+        String date="September 08, 2025";
+        System.out.println("Name:");
+        System.out.println(first);
+        System.out.println(last);
+        System.out.println("Date");
+        System.out.println(date);
+    }
+}
