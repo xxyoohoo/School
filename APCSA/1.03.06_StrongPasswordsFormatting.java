1a1fb99288d120757a2545b6e346b0ef0461cee0
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-10 20:48:16 -0400

    Create 1.03.06_StrongPasswordsFormatting.java

---
+++ APCSA/1.03.06_StrongPasswordsFormatting.java
@@ -0,0 +1,9 @@
+public class StrongPasswords
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Length\nThe longer a password is, the more secure it is.\n");
+        System.out.println("Complex Characters\nStrong passwords include a mix of uppercase and lowercase letters, numbers and special characters.\n");
+        System.out.print("Unpredictability\nPasswords should avoid using common words, phrases, and personal information.");
+    }
+}
