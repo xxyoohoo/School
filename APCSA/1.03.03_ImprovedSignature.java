d7d33c3296c3751a11a79b6a5e276c4b979c1316
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-10 20:47:24 -0400

    Create 1.03.03_ImprovedSignature.java

---
+++ APCSA/1.03.03_ImprovedSignature.java
@@ -0,0 +1,18 @@
+public class Signature
+{
+    public static void main(String[] args)
+    {
+        String firstName = "Ada";
+        String lastName = "Lovelace";
+        String date = "August 10, 1842";
+        
+        // Use print and println commands to print the name
+        System.out.print("Name: ");
+        System.out.println(firstName + ' ' + lastName);
+        
+        // Use print and println commands to print the date
+        System.out.print("Date: ");
+        System.out.println(date);
+        
+    }
+}
