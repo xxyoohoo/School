43a67157acb3abf25892caeb73f6085c726ba0fe
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-10 20:49:42 -0400

    Create 1.03.11_WeightOfAPyramid.java

---
+++ APCSA/1.03.11_WeightOfAPyramid.java
@@ -0,0 +1,10 @@
+public class Pyramid 
+{
+    public static void main(String[] args)
+    {
+        double blockWeight=2.5;
+        int numBlocks=2500000;
+        System.out.println("Pyramid weight in tons");
+        System.out.print(blockWeight*numBlocks);
+    }
+}
