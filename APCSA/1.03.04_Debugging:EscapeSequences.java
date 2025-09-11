5da3378e22bde8fb7c2a6934f447c6c3b56d5e66
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-10 20:47:52 -0400

    Create 1.03.04_Debugging:EscapeSequences.java

---
+++ APCSA/1.03.04_Debugging:EscapeSequences.java
@@ -0,0 +1,8 @@
+public class BuggyEscapes
+{
+    public static void main(String[] args)
+    {
+        System.out.print("\"Make it work,\nmake it right,\nmake it fast.\"\n");
+        System.out.println("\t-Kent Beck");
+    }
+}
