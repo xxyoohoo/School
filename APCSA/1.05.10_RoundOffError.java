5ff8bcb0c055cc6ee140c7ff729a2fc82a3d92f6
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-18 21:11:49 -0400

    Create 1.05.10_RoundOffError.java

---
+++ APCSA/1.05.10_RoundOffError.java
@@ -0,0 +1,10 @@
+public class RoundOff
+{
+    public static void main(String[] args)
+    {
+        double spd=0.034567;
+        int cvr=100000;
+        double ans=spd*cvr;
+        System.out.print(ans);
+    }
+}
