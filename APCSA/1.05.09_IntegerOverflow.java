c9815b60aee8f5b37f017122c569a45a5e1a02a9
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-09-18 21:11:25 -0400

    Create 1.05.09_IntegerOverflow.java

---
+++ APCSA/1.05.09_IntegerOverflow.java
@@ -0,0 +1,10 @@
+public class IntegerOverflow
+{
+    public static void main(String[] args)
+    {
+        int mn= Integer.MIN_VALUE;
+        int mx= Integer.MAX_VALUE;
+        System.out.println(mn+"\n"+mx+"\n"+(mn-1)+"\n"+(mx+1)+"\n"+(mx*2)+"\n"+(mn/2));
+        
+    }
+}
